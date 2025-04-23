5e77032f525e66fa8b6f4bd9271f90ced8c96bcc
Jiaxuan Yu <166388707+WZHDXXT@users.noreply.github.com>
2025-04-23 21:45:42 +0200

    Add files via upload

--- evaluation.py
+++ evaluation.py
@@ -2,41 +2,6 @@ import numpy as np
 import torch
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def evaluate_(model, user_train, user_valid, max_len, make_sequence_dataset, bert4rec_dataset):
-    model.eval()
-
-    NDCG = 0.0
-    HIT = 0.0
-    RECALL = 0.0
-
-    num_item_sample = 100
-    users = [user for user in range(make_sequence_dataset.num_user)]
-
-    for user in users:
-        # seq = (user_train[user] + [make_sequence_dataset.num_item + 1])[-max_len:]
-        seq = user_valid[user][-max_len:]
-        padding_len = max_len - len(seq)
-        seq = [0] * padding_len + seq
-
-        # rated = user_train[user] + [user_valid[user]]
-        rated = user_valid[user]
-        items = [user_valid[user][-1]] + bert4rec_dataset.random_neg_sampling(rated_items=rated, num_samples=num_item_sample)
-
-        with torch.no_grad():
-            seq = torch.LongTensor([seq]).to(device)
-            predictions = -model(seq)
-            predictions = predictions[0][-1][items]
-            rank = predictions.argsort().argsort()[0].item()
-            
-
-        if rank < 10:
-            NDCG += 1 / np.log2(rank + 2)
-            HIT += 1
-            RECALL += 1 / 1  # only 1 relevant item → recall = 1 if hit
-
-    total = len(users)
-    return NDCG / total, HIT / total, RECALL / total
-
 
 def evaluate(model, user_train, user_valid, max_len, make_sequence_dataset, bert4rec_dataset, K):
     model.eval()
@@ -79,3 +44,37 @@ def evaluate(model, user_train, user_valid, max_len, make_sequence_dataset, bert
 
     total = len(users)
     return NDCG / total, RECALL / total
+
+
+def full_ranking_evaluate_with_validation(model, user_train, user_valid, user_test, max_len, vocab_size, device, K=10):
+    model.eval()
+    recall_sum, ndcg_sum, user_count = 0.0, 0.0, 0
+
+    for user in user_test:
+        if len(user_train[user]) < 1 or len(user_test[user]) < 1:
+            continue
+
+        seq = user_train[user] + user_valid[user]  # train + valid 
+
+        for target_item in user_test[user]:
+            input_seq = (seq + [vocab_size - 1])[-max_len:]  
+            input_seq = [0] * (max_len - len(input_seq)) + input_seq
+
+            seq_tensor = torch.LongTensor([input_seq]).to(device)
+
+            with torch.no_grad():
+                logits = model(seq_tensor)  # [1, seq_len, vocab_size]
+                pred_scores = logits[0, -1] 
+
+            topk = torch.topk(pred_scores, k=K).indices.cpu().tolist()
+
+            if target_item in topk:
+                recall_sum += 1
+                rank = topk.index(target_item)
+                ndcg_sum += 1 / torch.log2(torch.tensor(rank + 2.0)).item()
+
+            seq.append(target_item)
+
+        user_count += 1
+
+    return ndcg_sum / user_count, recall_sum / user_count
